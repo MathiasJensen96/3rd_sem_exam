2ee6c79e67d00f346d1abd8e9a79ee3bb29faff3
Mathias Jensen <cph-mj839@chpbusiness.dk>
2022-01-14 01:34:43 +0100

    final commit?

--- src/test/java/facades/FacadeExampleTest.java
+++ src/test/java/facades/FacadeExampleTest.java
@@ -1,5 +1,9 @@
 package facades;
 
+import entities.Guide;
+import entities.Role;
+import entities.Trip;
+import entities.User;
 import org.junit.jupiter.api.*;
 import utils.EMF_Creator;
 
@@ -9,7 +13,7 @@ import javax.persistence.EntityManagerFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //Uncomment the line below, to temporarily disable this test
-@Disabled
+//@Disabled
 public class FacadeExampleTest {
 
     private static EntityManagerFactory emf;
@@ -49,6 +53,37 @@ public class FacadeExampleTest {
     //TODO -- Make sure to change the code below to use YOUR OWN entity class
     @BeforeEach
     public void setUp() {
+        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactoryForTest();
+        EntityManager em = emf.createEntityManager();
+
+        User user = new User("Mathias", "testHest", "Æblegade 8", 11111111, "Mathias@cphbusiness.dk", 1996, "male");
+        User admin = new User("Jon", "testHest", "Mortensgade 97", 22222222, "Jon@cphbusiness.dk", 1965, "male");
+        Trip trip = new Trip("Grand Canyon Tour", "Jan. 31st", "8 am", "Hotel Lobby", "Full day", "Packing List URL" );
+        Trip trip1 = new Trip("Skiing in Chamonix", "Feb. 7th", "7.30 am", "Local bus stop", "7 days", "Packing List URL" );
+        Guide guide = new Guide("Sofus", "male", 1993, "Profile URL", "image URL");
+        Guide guide1 = new Guide("Æske", "male", 1991, "Profile URL", "image URL");
+
+        if(admin.getUserPass().equals("test")||user.getUserPass().equals("test"))
+            throw new UnsupportedOperationException("You have not changed the passwords");
+
+        em.getTransaction().begin();
+        Role userRole = new Role("user");
+        Role adminRole = new Role("admin");
+        user.addRole(userRole);
+        admin.addRole(adminRole);
+        user.addTrip(trip);
+        user.addTrip(trip1);
+        trip.setGuide(guide);
+        trip1.setGuide(guide1);
+        em.persist(userRole);
+        em.persist(adminRole);
+        em.persist(user);
+        em.persist(admin);
+        em.persist(trip);
+        em.persist(trip1);
+        em.persist(guide);
+        em.persist(guide1);
+        em.getTransaction().commit();
     }
 
     @AfterEach
@@ -57,14 +92,11 @@ public class FacadeExampleTest {
     }
 
     // TODO: Delete or change this method 
-//    @Test
-//    public void testCreateUser() {
-//        facade = UserFacade.getUserFacade(emf);
-//        User actual = facade.createUser("thias", "thias123");
-//        User expected = new User("thias", "thias123");
-//        Role userRole = new Role("user");
-//        expected.addRole(userRole);
-//        assertEquals(expected.getUserName(), actual.getUserName());
-//    }
+    @Test
+    public void getAllUsersTest() {
+        String actual = facade.getAllUsers();
+        String expected = "[2]";
+        assertEquals(expected, actual);
+    }
 
 }
